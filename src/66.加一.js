f4ef5e31891fd4c6ae7df59843290005bdda4d3b
inmWang <inmWang@github.com>
2020-08-13 10:02:41 +0800

    commit 66

---
+++ src/66.加一.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=66 lang=javascript
+ *
+ * [66] 加一
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  let num = BigInt(digits.join('')) + 1n
+  return num.toString().split('')
+};
+// @lc code=end
+
